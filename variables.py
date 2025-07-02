12c1c15679fdb18bf1736d9054fbdd364299b84a
Luciana-IADevelop <lfzavagno@gmail.com>
2025-07-02 17:34:35 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+nombre = "Luciana" #str
+edad = 41 #int
+altura = 1.70 #float
+es_estudiante = true #bool
+
+print(nombre)
+print(edad)
+print(altura)
+print(es_estudiante)
+
+edad = 29
+print("Ahora tengo", edad)
+
+nombre = "TuNombre"
+ciudad = "TuCiudad"
+hobby = "TuHobby"
+print("Hola, soy", nombre, "y vivo en", ciudad, ". Me gusta", hobby)
